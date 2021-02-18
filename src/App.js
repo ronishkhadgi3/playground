e56464a660dc8d105748bde38e8c3815440b3b06
RonishKhadgi <60719674+RonishKhadgi@users.noreply.github.com>
2021-02-18 17:40:33 -0500

    Adding exercise 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Route, BrowserRouter as Router } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <img src = 'https://epicsports.cachefly.net/images/52253/300/fan-mats-nba-boston-celtics-court-runner.jpg'/>
+        <img src = "Championship.png" width = "240" px height = "170" px />
+      
+        <h1> Curriculm</h1>
+        <h3> Useful Link</h3>
+       <p>Set up Call with Instructor<a href="https://calendly.com" target = '_blank' rel = "noopener noreferrer"> Here</a> or email sachin@wynisco.com</p>
+       
+        <h2> Core Topics</h2>
+        <h2> Welcome to the Course </h2>
+        <h4> Application Architecuture</h4>
+        
+ </header>
     </div>
   );
 }
